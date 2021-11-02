7c82cc7f8ce0fca90ea5a2e9462133d7e1685e65
leonhardt <15501388056@163.com>
2021-11-02 18:04:46 +0800

    Initial commit

---
+++ loaddd.py
@@ -0,0 +1,9 @@
+import os
+import sys
+rootpath=str("D:/_1work/pycharmcode/pycharmproject/resrep")
+syspath=sys.path
+sys.path=[]
+sys.path.append(rootpath)#将工程根目录加入到python搜索路径中
+sys.path.extend([rootpath+i for i in os.listdir(rootpath) if i[0]!="."])#将工程目录下的一级目录添加到python搜索路径中
+sys.path.extend(syspath)
+print(sys.path)
